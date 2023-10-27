7d5d76c4a1107a3fe2074242ad1d1dbc4a347c86
Raka Okto Ramadhan <rakaoktoramadhan@gmail.com>
2023-10-27 18:14:31 +0700

    Update Import

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDrag } from "react-dnd";
-import "./item.css";
+import "./Item.css";
 import { formatDate } from "../composables/getTime";
 
 const Item = ({ data }: { data: any }) => {
